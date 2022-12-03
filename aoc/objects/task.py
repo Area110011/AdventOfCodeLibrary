a0db10081736143f2f760f2750664ba1dc63206b
Libor Šafář <liborsaf@wetian.eu>
2022-12-03 01:24:12 +0100

    Fix pytest warning & suppress ide outside init warning
    Took 10 minutes

--- aoc/objects/task.py
+++ aoc/objects/task.py
@@ -1,13 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import Optional
 
 
 class AdventOfCodeTask(ABC):
-    def __init__(self):
-        self.task_day = 0
-        self.task_year = 0
-        self.task_input: Optional[str] = None
-
+    # noinspection PyAttributeOutsideInit
     def fill(self, task_day: int, task_year: int, task_input: str):
         self.task_day = task_day
         self.task_year = task_year
